18d8c6026ac3a6adca4f54821462edbc70c83c82
tiannian <dtiannian@aliyun.com>
2018-01-27 19:48:07 +0800

    make test expect dependentices

--- LibKBT.cmake
+++ LibKBT.cmake
@@ -1,9 +1,5 @@
 macro(KBT_PROJECT)
     project(${ARGV0})
-    add_custom_target(init cmake -D KBT_VAR_SOURCE_DIR=${CMAKE_SOURCE_DIR}
-                  -D KBT_VAR_TOOLS_DIR=${CMAKE_BINARY_DIR}/KBT-${KBT_VAR_BRANCH}
-                  -D KBT_VAR_PROJECT_NAME=${PROJECT_NAME}
-                  -P ${KBT_VAR_TOOLS_DIR}/lib/init.cmake)
 endmacro()
 
 macro(KBT_SET_ARCH)
@@ -30,36 +26,48 @@ macro(KBT_SET_PROJECT_TYPE)
     endif()
 endmacro()
 
-macro(KBT_ADD_DEPENDENTICES dep version)
-    file(GLOB kbt_file_status "${CMAKE_SOURCE_DIR}/dependenices/${dep}")
-    if(NOT kbt_file_status)
-        set(kbt_file_status "git clone --recursive https://github.com/${dep}.git ${CMAKE_SOURCE_DIR}/dependenices/${dep}")
-        exec_program(${kbt_file_status})
-        file(COPY "${CMAKE_SOURCE_DIR}/dependenices/${dep}/include/" DESTINATION "${CMAKE_BINARY_DIR}/include/${dep}")
-    endif(NOT kbt_file_status)
+macro(KBT_ADD_DEPENDENTICES)
+    file(GLOB KBT_VAR_DEPENDIENCE_EXISTS "${CMAKE_SOURCE_DIR}/dependenices/${dep}")
+    # check remote repo version and exist
+    KBT_FUNC_CHECK_REMOTE_REPO_VERSION(${ARGV0} ${ARGV1})
+    if(NOT KBT_VAR_DEPENDIENCE_EXISTS)
+        execute_process(COMMAND git clone --recursive https://github.com/${dep}.git ${CMAKE_SOURCE_DIR}/dependenices/${dep})
+        file(COPY ${CMAKE_SOURCE_DIR}/dependenices/${dep}/include DESTINATION ${CMAKE_BINARY_DIR}/include/${dep})
+    endif()
     add_subdirectory("${CMAKE_SOURCE_DIR}/dependenices/${dep}")
-    include_directories("${CMAKE_BINARY_DIR}/include")
+    # include_directories("${CMAKE_BINARY_DIR}/include")
     if(KBT_${PROJECT_NAME}_TYPE STREQUAL "lib")
         list(APPEND LIB_DEPENDENTICES ${PROJECT_NAME})
     endif(KBT_${PROJECT_NAME}_TYPE STREQUAL "lib")
 endmacro()
 
-macro(kbt_config)
-    file(GLOB kbt_file_status ${PROJECT_SOURCE_DIR}/${PROJECT_NAME}.config)
-    if(NOT kbt_file_status)
+macro(KBT_CONFIG)
+    file(GLOB KBT_VAR_CONFIG_EXISTS ${PROJECT_SOURCE_DIR}/${PROJECT_NAME}.config)
+    if(NOT KBT_VAR_CONFIG_EXISTS)
         # Create test file
-        file(WRITE "${PROJECT_SOURCE_DIR}/${PROJECT_NAME}.config" "#cmakedefine PROJECT_NAME ${PROJECT_NAME}\n" )
         file(APPEND "${PROJECT_SOURCE_DIR}/${PROJECT_NAME}.config" "#cmakedefine KBT_ARCH ${KBT_ARCH}\n")
         file(APPEND "${PROJECT_SOURCE_DIR}/${PROJECT_NAME}.config" "#cmakedefine KBT_PLATFORM ${KBT_PLATFORM}\n")
         file(APPEND "${PROJECT_SOURCE_DIR}/${PROJECT_NAME}.config" "#cmakedefine KBT_${PROJECT_NAME}_TYPE ${KBT_${PROJECT_NAME}_TYPE}\n")
-    endif(NOT kbt_file_status)
-    configure_file("${PROJECT_NAME}.config" "${PROJECT_SOURCE_DIR}/include/config.h")
-    file(GLOB_RECURSE ${PROJECT_NAME}_srcs_list "${PROJECT_SOURCE_DIR}/src/*.c")
-    if(KBT_${PROJECT_NAME}_TYPE STREQUAL "lib")
-        add_library(${PROJECT_NAME} STATIC ${${PROJECT_NAME}_srcs_list})
-    endif(KBT_${PROJECT_NAME}_TYPE STREQUAL "lib")
-    if(KBT_${PROJECT_NAME}_TYPE STREQUAL "bin")
-        add_executable(${PROJECT_NAME} ${${PROJECT_NAME}_srcs_list})
-        target_link_libraries(${PROJECT_NAME} ${LIB_DEPENDENTICES})
-    endif(KBT_${PROJECT_NAME}_TYPE STREQUAL "bin")
-endmacro(kbt_config)
+        configure_file("${PROJECT_NAME}.config" "${PROJECT_SOURCE_DIR}/include/config.h")
+    endif()
+    # Scan all source file
+    file(GLOB_RECURSE KBT_VAR_SOURCES_FILES_LIST "${PROJECT_SOURCE_DIR}/src/*.c")
+    if(KBT_VAR_SOURCES_FILES_LIST)
+        include_directories(${CMAKE_SOURCE_DIR}/include)
+        if(KBT_${KBT_VAR_PROJECT}_TYPE STREQUAL "lib")
+            add_library(${PROJECT_NAME} STATIC ${KBT_VAR_SOURCES_FILES_LIST})
+        endif()
+        # Build bin project
+        if(KBT_${KBT_VAR_PROJECT}_TYPE STREQUAL "bin")
+            add_executable(${PROJECT_NAME} ${KBT_VAR_SOURCE_DIR)
+            # target_link_libraries(${PROJECT_NAME} ${LIB_DEPENDENTICES})
+        endif()
+    endif()
+    # Add init command in makefile
+    add_custom_target(init cmake -D KBT_VAR_SOURCE_DIR=${CMAKE_SOURCE_DIR}
+                  -D KBT_VAR_TOOLS_DIR=${CMAKE_BINARY_DIR}/KBT-${KBT_VAR_BRANCH}
+                  -D KBT_VAR_PROJECT_NAME=${PROJECT_NAME}
+                  -D KBT_VAR_PROJECT_TYPE=${KBT_${KBT_VAR_PROJECT}_TYPE}
+                  -P ${KBT_VAR_TOOLS_DIR}/lib/init.cmake
+                  COMMAND make rebuild_cache)
+endmacro()
